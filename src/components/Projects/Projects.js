466f9d8766cd1196e8e1117c4e7b4ba85c69a998
deshmukh612 <99964682+deshmukh612@users.noreply.github.com>
2022-05-28 21:29:13 +0530

    images of projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
-// import indiaMart from "../../Assets/Projects/indiaMart.jpg"
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.jpeg";
+// import editor from "../../Assets/Projects/codeEditor.png";
+// import chatify from "../../Assets/Projects/chatify.png";
+// import suicide from "../../Assets/Projects/suicide.png";
+// import bitsOfCode from "../../Assets/Projects/blog.png";
+import indiaMart from "../../Assets/Projects/indiaMart.jpg"
+import meesho from "../../Assets/Projects/meesho.jpg"
+import naukari from "../../Assets/Projects/naukari.jpg"
 
 
 function Projects() {
@@ -25,7 +27,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={indiaMart}
               isBlog={false}
               title="IndiaMart"
               description="IndiaMART is online B2B marketplace, connecting buyers with suppliers, the channel focuses on providing a platform to Small & Medium Enterprises (SMEs), Large Enterprises as well as individuals build with react.js, Chakra-UI library. Have features which allows buyers to contact seller about requirement of various product."
@@ -35,7 +37,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={meesho}
               isBlog={false}
               title="Meesho"
               description="The ecommerce website to purchase cholthing appreals build using html, css and javascript with gallery of different products, detailing and payment gateway."
@@ -45,7 +47,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={naukari}
               isBlog={false}
               title="Naukari.com"
               description="A web application to provide platform for job-seeker, search for vacancies and apply for it.Online Editor which supports html, css, and js code with instant view of website and supports auto save of work using Local Storage"
